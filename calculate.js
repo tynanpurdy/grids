e99cbdc5b50163e996fa6d5e2e1edab79faeca1c
Tynan Purdy <Tynan.Purdy@cognizant.com>
2022-06-12 20:02:29 -0500

    progress!

--- calculate.js
+++ calculate.js
@@ -4,20 +4,23 @@
 
 //params
 const lines_per_field = 2
-const lines_per_gutter = 0
-const h_fields = 1
-const v_fields = 1
-const box_height = 21.897
-const box_width = 678.858
-const h_text = 7.084
+// const lines_per_gutter = 0
+const rows = document.getElementById('rows').value
+const columns = document.getElementById('columns').value
+const box_height = 21.897 // calculate
+const box_width = 678.858 // calculate
+const h_text = 7.084 // measure??
 
 //calcs
-const lines = lines_per_field * h_fields + lines_per_gutter * (h_fields - 1)
+const lines = lines_per_field * rows + lines_per_gutter * (rows - 1)
 const total_text = h_text * lines
 const per_line_spacing = (box_height - total_text) / (lines - 1)
 
 //output
-const leading = per_line_spacing + h_text
+function getLeading() {
+    return per_line_spacing + h_text
+}
+const leading = getLeading()
 const h_gutter = 2 * per_line_spacing + h_text
-const h_field = (box_height - h_gutter * (h_fields - 1)) / h_fields
-const v_gutter = (box_width - v_fields * h_field) / (v_fields - 1)
+const h_field = (box_height - h_gutter * (rows - 1)) / rows
+const v_gutter = (box_width - columns * h_field) / (columns - 1)
